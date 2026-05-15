539f161d12fdd0e220d85bf569925cf2a40c1211
Hasan Mustafa <hasan.mustafa.ds@gmail.com>
2026-05-15 16:23:01 +0530

    auth

--- backend/services/auth/config.py
+++ backend/services/auth/config.py
@@ -1,8 +1,17 @@
 # backend/auth/config.py
 from datetime import timedelta
+from dotenv import load_dotenv
 
-SECRET_KEY = "CHANGE_THIS_TO_SECURE_RANDOM_KEY"
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 60
+import os
+
+load_dotenv()
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = os.getenv("ALGORITHM", "HS256")
+
+ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 60))
 
 ACCESS_TOKEN_EXPIRE = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+
+REFRESH_TOKEN_EXPIRE_DAYS = int(os.getenv("REFRESH_TOKEN_EXPIRE_DAYS", 2))
+REFRESH_TOKEN_EXPIRE = timedelta(days=REFRESH_TOKEN_EXPIRE_DAYS)
